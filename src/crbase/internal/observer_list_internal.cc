5f614e95f740f9ebdfd685eefb88396284f32140
Ninja2ha <115858785+ninja2ha@users.noreply.github.com>
2026-01-26 11:06:43 +0800

    add chromium version information.

--- src/crbase/internal/observer_list_internal.cc
+++ src/crbase/internal/observer_list_internal.cc
@@ -1,6 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// * VERSION: 91.0.4472.169
 
 #include "crbase/internal/observer_list_internal.h"
 
--- src/crbase/internal/observer_list_internal.h
+++ src/crbase/internal/observer_list_internal.h
@@ -1,6 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// * VERSION: 91.0.4472.169
 
 #ifndef MINI_CHROMIUM_SRC_CRBASE_INTERNAL_OBSERVER_LIST_INTERNAL_H_
 #define MINI_CHROMIUM_SRC_CRBASE_INTERNAL_OBSERVER_LIST_INTERNAL_H_
