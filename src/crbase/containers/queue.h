1de1bec7f48c4bf2a24231fb5b881da38450465f
Ninja2ha <115858785+ninja2ha@users.noreply.github.com>
2026-01-30 16:51:34 +0800

    [crbase]: write chromium version.,

--- src/crbase/containers/queue.h
+++ src/crbase/containers/queue.h
@@ -1,6 +1,7 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// * VERSION: 91.0.4472.169
 
 #ifndef MINI_CHROMIUM_SRC_CRBASE_CONTAINERS_QUEUE_H_
 #define MINI_CHROMIUM_SRC_CRBASE_CONTAINERS_QUEUE_H_
